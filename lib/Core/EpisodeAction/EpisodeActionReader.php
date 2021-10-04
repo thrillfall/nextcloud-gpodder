445cf92a980052760247df8ac5a4f5ea64d71ee7
Jonathan Flueren <11487762+JonOfUs@users.noreply.github.com>
2021-10-04 17:59:05 +0200

    Allow EpisodeAction upload as array
    Parsing EpisodeActions from String can lead to problems if the String is changed by only a little bits.
    Additionally, the gpodder api describes the upload as Array.
    Uploading EpisodeActions as String is still possible.

--- lib/Core/EpisodeAction/EpisodeActionReader.php
+++ lib/Core/EpisodeAction/EpisodeActionReader.php
@@ -53,4 +53,28 @@ class EpisodeActionReader
 		}
 		return $episodeActions;
 	}
+
+	/**
+	 * @param $episodeActionsArray[]
+	 * @return EpisodeAction[]
+	 */
+	public function fromArray(array $episodeActionsArray): array
+	{
+		$episodeActions = [];
+
+		foreach($episodeActionsArray as $episodeAction) {
+			$episodeActions[] = new EpisodeAction(
+				$episodeAction["podcast"],
+				$episodeAction["episode"],
+				$episodeAction["action"],
+				$episodeAction["timestamp"],
+				$episodeAction["started"],
+				$episodeAction["position"],
+				$episodeAction["total"],
+				$episodeAction["guid"] ?? null,
+			);
+		}
+
+		return $episodeActions;
+	}
 }
