80fbe44505b5c55f730e85520797a9dac0bee258
Jonathan Flueren <11487762+JonOfUs@users.noreply.github.com>
2021-10-05 14:57:49 +0200

    Adjust EpisodeAction upload to gpodder api, remove String upload
    Remove 3rd dimension of POST data by directly accessing request data - a simple EpisodeActions array has to be posted now. This way the api is closer to gpodder.
    Additionally, removed EpisodeAction upload as String, so it's now incompatible with older versions

--- lib/Core/EpisodeAction/EpisodeActionReader.php
+++ lib/Core/EpisodeAction/EpisodeActionReader.php
@@ -5,55 +5,6 @@ namespace OCA\GPodderSync\Core\EpisodeAction;
 
 class EpisodeActionReader
 {
-
-	const EPISODEACTION_IDENTIFIER = 'EpisodeAction{';
-
-	/**
-	 * @param string $episodeActionsString
-	 * @return EpisodeAction[]
-	 */
-	public function fromString(string $episodeActionsString): array
-	{
-
-
-		$patterns = [
-			'/EpisodeAction{(podcast=\')(?<podcast>.*?)(\', episode=\')(?<episode>.*?)(\', guid=\')(?<guid>.*?)(\', action=)(?<action>.*?)(, timestamp=)(?<timestamp>.*?)(, started=)(?<started>.*?)(, position=)(?<position>.*?)(, total=)(?<total>.*?)}]*/',
-			'/EpisodeAction{(podcast=\')(?<podcast>.*?)(\', episode=\')(?<episode>.*?)(\', action=)(?<action>.*?)(, timestamp=)(?<timestamp>.*?)(, started=)(?<started>.*?)(, position=)(?<position>.*?)(, total=)(?<total>.*?)}]*/',
-		];
-
-		$episodeActions = [];
-
-		$episodeActionStrings = explode(self::EPISODEACTION_IDENTIFIER, $episodeActionsString);
-		array_shift($episodeActionStrings);
-
-		foreach ($episodeActionStrings as $episodeActionString) {
-			foreach ($patterns as $pattern) {
-				preg_match(
-					$pattern,
-					self::EPISODEACTION_IDENTIFIER . $episodeActionString,
-					$matches
-				);
-
-				if ($matches["action"] !== null) {
-					$episodeActions[] = new EpisodeAction(
-						$matches["podcast"],
-						$matches["episode"],
-						$matches["action"],
-						$matches["timestamp"],
-						(int)$matches["started"],
-						(int)$matches["position"],
-						(int)$matches["total"],
-						$matches["guid"] ?? null,
-						null,
-					);
-					break;
-				}
-			}
-
-		}
-		return $episodeActions;
-	}
-
 	/**
 	 * @param $episodeActionsArray[]
 	 * @return EpisodeAction[]
