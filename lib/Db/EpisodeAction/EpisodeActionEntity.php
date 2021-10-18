aa1ec92ada31889363dc0734c46deccecf206347
Matthias Gutjahr <mattsches@gmail.com>
2021-10-18 10:55:00 +0200

    Add phpdoc @method tags for setters

--- lib/Db/EpisodeAction/EpisodeActionEntity.php
+++ lib/Db/EpisodeAction/EpisodeActionEntity.php
@@ -8,14 +8,23 @@ use OCP\AppFramework\Db\Entity;
 
 /**
  * @method string getPodcast()
+ * @method void setPodcast(string $podcast)
  * @method string getEpisode()
+ * @method void setEpisode(string $episode)
  * @method string getAction()
+ * @method void setAction(string $action)
  * @method integer getTimestampEpoch()
+ * @method void setTimestampEpoch(mixed $timestampEpoch)
  * @method integer getStarted()
+ * @method void setStarted(integer $started)
  * @method integer getPosition()
+ * @method void setPosition(integer $position)
  * @method integer getTotal()
+ * @method void setTotal(integer $total)
  * @method string getGuid()
+ * @method void setGuid(string $guid)
  * @method string getUserId()
+ * @method void setUserId(string $userId)
  */
 class EpisodeActionEntity extends Entity implements JsonSerializable {
 
