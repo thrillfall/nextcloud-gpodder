33dd043dcb889a25b5d5802863be9ad22c7254ef
Kalle Fagerberg <kalle.f8@proton.me>
2022-07-17 18:23:16 +0200

    Added fetched podcast data

--- src/personalSettings.js
+++ src/personalSettings.js
@@ -8,6 +8,10 @@ __webpack_public_path__ = generateFilePath(appName, '', 'js/')
 
 Vue.mixin({ methods: { t, n } })
 
+// https://nextcloud-vue-components.netlify.app/#/Introduction
+Vue.prototype.OC = window.OC
+Vue.prototype.OCA = window.OCA
+
 export default new Vue({
 	el: '#personal_settings',
 	render: h => h(PersonalSettingsPage),
--- src/views/PersonalSettingsPage.vue
+++ src/views/PersonalSettingsPage.vue
@@ -3,16 +3,59 @@
 		<SettingsSection :title="t('gpoddersync', 'Synced subscriptions')"
 			:description="t('gpoddersync', 'Podcast subscriptions that has so far been synchronized with this Nextcloud account.')">
 			<span>Hello <span class="red_text">world</span> :)</span>
+			<ul>
+				<ListItem v-for="sub in subscriptions"
+					:key="sub.url"
+					:title="sub.podcast?.title ?? sub.url">
+					<template #icon>
+						<Avatar :size="44"
+							:url="sub.podcast?.image"
+							:display-name="sub.podcast?.author" />
+					</template>
+					<template #subtitle>
+						{{ sub.podcast?.description }}
+					</template>
+				</ListItem>
+			</ul>
 		</SettingsSection>
 	</div>
 </template>
 
 <script>
-import { SettingsSection } from '@nextcloud/vue'
+import Avatar from '@nextcloud/vue/dist/Components/Avatar'
+import ListItem from '@nextcloud/vue/dist/Components/ListItem'
+import SettingsSection from '@nextcloud/vue/dist/Components/SettingsSection'
+
+import { generateUrl } from '@nextcloud/router'
+import axios from '@nextcloud/axios'
+import { showError } from '@nextcloud/dialogs'
+
 export default {
 	name: 'PersonalSettingsPage',
 	components: {
+		Avatar,
+		ListItem,
 		SettingsSection,
+	},
+	data() {
+		return {
+			subscriptions: [],
+			isLoading: true,
+		}
+	},
+	async mounted() {
+		try {
+			const resp = await axios.get(generateUrl('/apps/gpoddersync/personal_settings/metrics'))
+			if (!Array.isArray(resp.data.subscriptions)) {
+				throw new Error('expected subscriptions array in metrics response')
+			}
+			this.subscriptions = resp.data.subscriptions
+		} catch (e) {
+			console.error(e)
+			showError(t('gpoddersync', 'Could not fetch podcast synchronization stats'))
+		} finally {
+			this.isLoading = false
+		}
 	},
 }
 </script>
