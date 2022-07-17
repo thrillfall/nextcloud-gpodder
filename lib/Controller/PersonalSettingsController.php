2b34249862ca2f150999bf3ab3fad7c11e80b7fe
Kalle Fagerberg <kalle.f8@proton.me>
2022-07-17 18:23:16 +0200

    Added fetched podcast data

--- lib/Controller/PersonalSettingsController.php
+++ lib/Controller/PersonalSettingsController.php
@@ -4,34 +4,43 @@ declare(strict_types=1);
 namespace OCA\GPodderSync\Controller;
 
 use DateTime;
+
+use OCA\GPodderSync\Service\PodcastCacheService;
 use OCA\GPodderSync\Db\EpisodeAction\EpisodeActionRepository;
 use OCA\GPodderSync\Db\SubscriptionChange\SubscriptionChangeEntity;
 use OCA\GPodderSync\Db\SubscriptionChange\SubscriptionChangeRepository;
-use OCP\AppFramework\Http\TemplateResponse;
+
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\JSONResponse;
 use OCP\IL10N;
 use OCP\IRequest;
 use OCP\Settings\ISettings;
 
-class EpisodeActionController extends Controller {
+use Psr\Log\LoggerInterface;
 
-	private IL10N $l;
+class PersonalSettingsController extends Controller {
+
+	private LoggerInterface $logger;
+	private string $userId;
 	private SubscriptionChangeRepository $subscriptionChangeRepository;
 	private EpisodeActionRepository $episodeActionRepository;
-	private string $userId;
+	private PodcastCacheService $podcastCacheService;
 
 	public function __construct(
 		string $AppName,
 		IRequest $request,
-		$UserId,
-		IL10N $l,
+		LoggerInterface $logger,
+		string $UserId,
 		SubscriptionChangeRepository $subscriptionChangeRepository,
 		EpisodeActionRepository $episodeActionRepository,
+		PodcastCacheService $podcastCacheService,
 	) {
 		parent::__construct($AppName, $request);
-		$this->l = $l;
+		$this->logger = $logger;
+		$this->userId = $UserId ?? '';
 		$this->subscriptionChangeRepository = $subscriptionChangeRepository;
 		$this->episodeActionRepository = $episodeActionRepository;
-		$this->userId = $UserId ?? '';
+		$this->podcastCacheService = $podcastCacheService;
 	}
 
 	/**
@@ -43,28 +52,64 @@ class EpisodeActionController extends Controller {
 	 */
 	public function metrics(): JSONResponse {
 		$sinceDatetime = (new DateTime)->setTimestamp(0);
-		$subscriptions = $this->extractUrlList($this->subscriptionChangeRepository->findAllSubscribed($sinceDatetime, $this->userId));
+		$subscriptionChanges = $this->subscriptionChangeRepository->findAllSubscribed($sinceDatetime, $this->userId);
 		$episodeActions = $this->episodeActionRepository->findAll(0, $this->userId);
+
 		$subStats = array();
-		foreach ($episodeActions as $action) {
-			$pod = $action->getPodcast();
-			$sub = $subStats[$pod] ?? array();
-			$sub['started']++;
-			$subStats[$pod] = $sub;
+		foreach ($episodeActions as $ep) {
+			$url = $ep->getPodcast();
+			$stats = $subStats[$url] ?? [
+				'listenedSeconds' => 0,
+				'actionCounts' => $this->defaultActionCounts(),
+			];
+			$actionCounts = $stats['actionCounts'];
+			$actionLower = strtolower($ep->getAction());
+			if (array_key_exists($actionLower, $actionCounts)) {
+				$actionCounts[$actionLower]++;
+			}
+			$stats['actionCounts'] = $actionCounts;
+			if ($actionLower == 'play') {
+				$seconds = $ep->getPosition();
+				if ($seconds && $seconds != -1) {
+					$stats['listenedSeconds'] += $seconds;
+				}
+			}
+			$subStats[$url] = $stats;
 		}
+
+		$subscriptions = array_map(function (SubscriptionChangeEntity $sub) use ($subStats) {
+			$url = $sub->getUrl();
+			$stats = $subStats[$url] ?? array();
+			$sub = [
+				'url' => $url ?? '',
+				'listenedSeconds' => $stats['listenedSeconds'],
+				'actionCounts' => $stats['actionCounts'],
+			];
+			try {
+				$podcast = $this->podcastCacheService->getCachedOrFetchPodcastData($url);
+				$sub['podcast'] = $podcast;
+			} catch (Exception $e) {
+				$sub['podcast'] = null;
+				$this->logger->error("Failed to get podcast data.", [
+					'exception' => $e,
+					'podcastUrl' => $url,
+				]);
+			}
+			return $sub;
+		}, $subscriptionChanges);
+
 		return new JSONResponse([
 			'subscriptions' => $subscriptions,
-			'subStats' => $subStats,
 		]);
 	}
 
-	/**
-	 * @param array $allSubscribed
-	 * @return mixed
-	 */
-	private function extractUrlList(array $allSubscribed): array {
-		return array_map(static function (SubscriptionChangeEntity $subscription) {
-			return $subscription->getUrl();
-		}, $allSubscribed);
+	private function defaultActionCounts(): array {
+		return [
+			'download' => 0,
+			'delete' => 0,
+			'play' => 0,
+			'new' => 0,
+			'flattr' => 0,
+		];
 	}
 }
