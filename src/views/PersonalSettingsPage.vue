7b889eb0fc3f78b388e53101fdec8b213764eee2
Kalle Fagerberg <kalle.f8@proton.me>
2022-07-17 20:08:08 +0200

    Added more info to list view

--- src/views/PersonalSettingsPage.vue
+++ src/views/PersonalSettingsPage.vue
@@ -2,29 +2,63 @@
 	<div class="gpoddersync_settings">
 		<SettingsSection :title="t('gpoddersync', 'Synced subscriptions')"
 			:description="t('gpoddersync', 'Podcast subscriptions that has so far been synchronized with this Nextcloud account.')">
-			<span>Hello <span class="red_text">world</span> :)</span>
-			<ul>
-				<ListItem v-for="sub in subscriptions"
-					:key="sub.url"
-					:title="sub.podcast?.title ?? sub.url">
+			<div v-if="subscriptions.length > 0">
+				<ul>
+					<ListItem v-for="sub in subscriptions"
+						:key="sub.url"
+						:title="sub.podcast?.title ?? sub.url"
+						:details="formatSubscriptionDetails(sub)">
+						<template #icon>
+							<Avatar :size="44"
+								:url="sub.podcast?.image"
+								:display-name="sub.podcast?.author" />
+						</template>
+						<template #subtitle>
+							{{ sub.podcast?.description }}
+						</template>
+						<template #actions>
+							<ActionLink :href="sub.podcast?.link"
+								target="_blank"
+								icon="icon-external">
+								Podcast's homepage
+							</ActionLink>
+							<ActionLink :href="sub.url"
+								target="_blank">
+								<template #icon>
+									<Rss />
+								</template>
+								RSS feed
+							</ActionLink>
+						</template>
+					</ListItem>
+				</ul>
+			</div>
+			<div v-if="subscriptions.length === 0 && !isLoading">
+				<EmptyContent>
+					No subscriptions
 					<template #icon>
-						<Avatar :size="44"
-							:url="sub.podcast?.image"
-							:display-name="sub.podcast?.author" />
+						<Podcast />
 					</template>
-					<template #subtitle>
-						{{ sub.podcast?.description }}
+					<template #desc>
+						Start syncing podcasts from your favorite podcast client, such as
+						<a class="link" href="https://antennapod.org/" target="_blank">Antennapod</a>,
+						and then refresh this page to see them pop up here.
 					</template>
-				</ListItem>
-			</ul>
+				</EmptyContent>
+			</div>
 		</SettingsSection>
 	</div>
 </template>
 
 <script>
+import ActionLink from '@nextcloud/vue/dist/Components/ActionLink'
 import Avatar from '@nextcloud/vue/dist/Components/Avatar'
+import EmptyContent from '@nextcloud/vue/dist/Components/EmptyContent'
 import ListItem from '@nextcloud/vue/dist/Components/ListItem'
 import SettingsSection from '@nextcloud/vue/dist/Components/SettingsSection'
+
+import Podcast from 'vue-material-design-icons/Podcast'
+import Rss from 'vue-material-design-icons/Rss.vue'
 
 import { generateUrl } from '@nextcloud/router'
 import axios from '@nextcloud/axios'
@@ -33,8 +67,12 @@ import { showError } from '@nextcloud/dialogs'
 export default {
 	name: 'PersonalSettingsPage',
 	components: {
+		ActionLink,
 		Avatar,
+		EmptyContent,
 		ListItem,
+		Podcast,
+		Rss,
 		SettingsSection,
 	},
 	data() {
@@ -57,11 +95,26 @@ export default {
 			this.isLoading = false
 		}
 	},
+	methods: {
+		formatSubscriptionDetails(sub) {
+			if (sub.listenedSeconds <= 0) {
+				return '(no time listened)'
+			}
+			const hours = Math.floor(sub.listenedSeconds / 3600)
+			const modMinutes = Math.floor(sub.listenedSeconds / 60) % 60
+			if (hours === 0) {
+				const modSeconds = sub.listenedSeconds % 60
+				return `(${modMinutes}min ${modSeconds}s listened)`
+			}
+			return `(${hours}h ${modMinutes}min listened)`
+		},
+	},
 }
 </script>
 
 <style lang="scss" scoped>
-.red_text {
-	color: red;
+a.link {
+	text-decoration: underline;
+	color: var(--color-primary-element-light);
 }
 </style>
