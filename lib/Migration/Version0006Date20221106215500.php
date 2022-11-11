60b51e8c96327dd9815aaa5d30bb1eb429ea67c7
JonOfUs <jonofus@flueren.eu>
2022-11-11 12:10:26 +0100

    Fix broken migration, lead to errors with too large db keys

--- lib/Migration/Version0006Date20221106215500.php
+++ lib/Migration/Version0006Date20221106215500.php
@@ -14,6 +14,13 @@ class Version0006Date20221106215500 extends \OCP\Migration\SimpleMigrationStep {
                 $schema = $schemaClosure();
 
                 $table = $schema->getTable('gpodder_subscriptions');
+
+                // hotfix due to errors with too long key lengths (https://github.com/thrillfall/nextcloud-gpodder/issues/103)
+                $table->dropIndex('subscriptions_url_user');
+                $table->addUniqueIndex(['url', "user_id"], 'subscriptions_url_user', [ 
+                        'lengths' => [ 500, 200 ]
+                ]);
+
                 $table->changeColumn('url', ['length' => 1000]);
 
                 return $schema;
