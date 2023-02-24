a36b53dce64992ef17124bcd2fcf05f94c44575b
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-24 14:44:16 +0100

    pbek/nextcloud-gpodder#1: Try to set CSP

--- lib/Settings/GPodderSyncPersonal.php
+++ lib/Settings/GPodderSyncPersonal.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 namespace OCA\GPodderSync\Settings;
 
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Http\ContentSecurityPolicy;
 use OCP\Settings\ISettings;
 
 class GPodderSyncPersonal implements ISettings {
 
 	public function getForm(): TemplateResponse {
-		return new TemplateResponse('gpoddersync', 'settings/personal', []);
+        $response = new TemplateResponse('gpoddersync', 'settings/personal', []);
+
+        // Try to set CSP to allow images and media from anywhere
+        $csp = new ContentSecurityPolicy();
+        $csp->addAllowedImageDomain('*')
+            ->addAllowedMediaDomain('*')
+            ->addAllowedConnectDomain('*');
+        $response->setContentSecurityPolicy($csp);
+
+        return $response;
 	}
 
 	public function getSection(): string {
